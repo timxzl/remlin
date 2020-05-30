f5dce28310bb568f24798aacaacb25d7f34cd065
timxzl <timxzl>
2020-05-30 17:44:15 -0400

    Now can match patches and a vertical stripe against another pic

--- remlin.py
+++ remlin.py
@@ -15,7 +15,7 @@ def VerticalBlur(arr):
 # arr: greyscale 2d np arrays from Image.convert("L"), blurred or not
 # returns: consecutive range of ints (width in total), indicating column numbers of the found vert line
 def FindVertLine(arr, width):
-  _, ncols = arr.shape
+  ncols = arr.shape[1]
   min_start = -1
   min_sum = sys.maxsize
   accu = np.sum(arr, axis=0)
@@ -28,3 +28,77 @@ def FindVertLine(arr, width):
 
 def TagVertLine(arr, col_indices, rgb):
   arr[:, np.array(col_indices)] = np.array(rgb)
+
+def TagRect(arr, top, bottom, left, right, rgb):
+  arr[top:top+3, left : right] = rgb
+  arr[bottom-3:bottom, left : right] = rgb
+  arr[top : bottom, left:left+3] = rgb
+  arr[top : bottom, right-3:right] = rgb
+
+def VertPad(arr, top_rows, bottom_rows):
+  avg = np.mean(arr, axis=(0,1)).astype(int)
+  top_shape = list(arr.shape)
+  top_shape[0] = top_rows
+  top = np.empty_like(arr, shape=top_shape)
+  top[:,:] = avg
+  bottom_shape = list(arr.shape)
+  bottom_shape[0] = bottom_rows
+  bottom = np.empty_like(arr, shape=bottom_shape)
+  bottom[:,:] = avg
+  return np.concatenate((top, arr, bottom), axis=0)
+
+def PatchDist(a, b, masked_cols = None):
+  diff = a-b
+  if masked_cols is not None:
+    diff[:,masked_cols] = 0
+  return np.sum(np.square(diff))
+
+def MatchPatch(patch, arr, row0, col0, row_range, col_range, masked_cols = None):
+  mindist = sys.maxsize
+  min_row, min_col = row0, col0
+  min_shift = 0
+  nrows = patch.shape[0]
+  ncols = patch.shape[1]
+  for drow in row_range:
+    for dcol in col_range:
+      row = row0 + drow
+      col = col0 + dcol
+      dist = PatchDist(patch, arr[row : row+nrows, col : col+ncols], masked_cols)
+      shift = drow*drow+dcol*dcol
+      if dist < mindist or (dist == mindist and shift < min_shift):
+        mindist = dist
+        min_shift = shift
+        min_row, min_col = row, col
+  return (min_row, min_col, mindist)
+
+# stripe: a vertical subrange of graph
+# arr must already be patched top and bottom, at least cover row_range
+def MatchStripe(stripe, step, arr, row0, col0, row_range, col_range, masked_cols = None,
+    colors = [(1, 200, 250), (250, 1, 200), (200, 250, 1)]):
+  tagged_arr = None
+  if colors is not None:
+    tagged_arr = np.copy(arr)
+
+  color_idx = 0
+  nrows = stripe.shape[0]
+  ncols = stripe.shape[1]
+  result = []
+  top = 0
+  while top < nrows:
+    bottom = top + step
+    if bottom > nrows:
+      bottom = nrows
+    patch = stripe[top:bottom]
+    min_row, min_col, mindist = MatchPatch(patch, arr, row0, col0, row_range, col_range, masked_cols)
+    result.append((min_row, min_col, mindist))
+    print("Match", top, ":", bottom, "is", min_row, min_col, mindist)
+    if colors is not None:
+      TagRect(tagged_arr, min_row, min_row+step, min_col, min_col+ncols, colors[color_idx])
+      TagRect(stripe, top, bottom, 0, ncols, colors[color_idx])
+      color_idx = (color_idx+1)%len(colors)
+    # Use this patch's position to calculate next patch's initial estimate position
+    row0 = min_row+step
+    col0 = min_col
+    top = bottom
+
+  return (result, tagged_arr)
